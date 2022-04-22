9b60298e82f9364ea19ef48031fa8c355db3abfe
Koki Shinjo <sktometometo@gmail.com>
2022-04-22 16:45:01 +0900

    [jsk_fetch_startup] add researching

--- jsk_fetch_robot/jsk_fetch_startup/scripts/track_single_person_node.py
+++ jsk_fetch_robot/jsk_fetch_startup/scripts/track_single_person_node.py
@@ -10,6 +10,8 @@ from jsk_recognition_msgs.msg import BoundingBox
 from jsk_recognition_msgs.msg import BoundingBoxArray
 from jsk_recognition_msgs.msg import ObjectArray
 
+from std_msgs.msg import Header
+
 from geometry_msgs.msg import PointStamped
 
 
@@ -24,6 +26,7 @@ class TrackSinglePersonNode(object):
             '~target_bbox', BoundingBox, queue_size=1)
 
         self.target_track_id = None
+        self.target_point = None
         self.time_target_track_lost = None
         self.param_target_track_duration = rospy.Duration(
             rospy.get_param('~target_track_duration', 3.0))
@@ -48,6 +51,62 @@ class TrackSinglePersonNode(object):
         self.time_clicked = msg.header.stamp
         self.point_clicked = msg
 
+    def get_nearest_bbox_index(self, msg_point_stamped, msg_bbox_array, target_search_distance):
+
+        try:
+            kdlframe_clicked_frame_to_bbox_frame = tf2_geometry_msgs.transform_to_kdl(
+                self.tf_buffer.lookup_transform(
+                    msg_point_stamped.header.frame_id,
+                    msg_bbox_array.header.frame_id,
+                    rospy.Time.now()
+                )
+            )
+        except (tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException) as e:
+            rospy.logerr(e)
+            return None
+        kdlframe_clicked_frame_to_clicked_point = PyKDL.Frame(
+            PyKDL.Rotation.Quaternion(0, 0, 0, 1),
+            PyKDL.Vector(
+                msg_point_stamped.point.x,
+                msg_point_stamped.point.y,
+                msg_point_stamped.point.z
+            )
+        )
+
+        def calc_distance_to_clicked_point(bbox):
+
+            kdlframe_bbox_frame_to_bbox_pose = PyKDL.Frame(
+                PyKDL.Rotation.Quaternion(
+                    bbox.pose.orientation.x,
+                    bbox.pose.orientation.y,
+                    bbox.pose.orientation.z,
+                    bbox.pose.orientation.w,
+                ),
+                PyKDL.Vector(
+                    bbox.pose.position.x,
+                    bbox.pose.position.y,
+                    bbox.pose.position.z,
+                )
+            )
+            kdlframe_clicked_point_to_box_pose = \
+                kdlframe_clicked_frame_to_clicked_point.Inverse() \
+                * kdlframe_clicked_frame_to_bbox_frame \
+                * kdlframe_bbox_frame_to_bbox_pose
+            return kdlframe_clicked_point_to_box_pose.p.Norm()
+
+        closest_index = msg_bbox_array.boxes.index(
+            min(msg_bbox_array.boxes,
+                key=calc_distance_to_clicked_point)
+        )
+
+        if calc_distance_to_clicked_point(msg_bbox_array.boxes[closest_index]) > target_search_distance:
+            rospy.logwarn('cannot find object')
+            return None
+
+        return closest_index
+
     def callback(self, msg_bbox_array, msg_obj_array):
 
         if self.point_clicked is not None:
@@ -59,60 +118,19 @@ class TrackSinglePersonNode(object):
             else:
                 if len(msg_bbox_array.boxes) == 0:
                     return
-                try:
-                    kdlframe_clicked_frame_to_bbox_frame = tf2_geometry_msgs.transform_to_kdl(
-                        self.tf_buffer.lookup_transform(
-                            self.point_clicked.header.frame_id,
-                            msg_bbox_array.header.frame_id,
-                            rospy.Time.now()
+
+                closest_index = self.get_nearest_bbox_index(
+                        self.point_clicked,
+                        msg_bbox_array,
+                        self.param_target_search_distance
                         )
-                    )
-                except (tf2_ros.LookupException,
-                        tf2_ros.ConnectivityException,
-                        tf2_ros.ExtrapolationException) as e:
-                    rospy.logerr(e)
-                    return
-                kdlframe_clicked_frame_to_clicked_point = PyKDL.Frame(
-                    PyKDL.Rotation.Quaternion(0, 0, 0, 1),
-                    PyKDL.Vector(
-                        self.point_clicked.point.x,
-                        self.point_clicked.point.y,
-                        self.point_clicked.point.z
-                    )
-                )
 
-                def calc_distance_to_clicked_point(bbox):
-
-                    kdlframe_bbox_frame_to_bbox_pose = PyKDL.Frame(
-                        PyKDL.Rotation.Quaternion(
-                            bbox.pose.orientation.x,
-                            bbox.pose.orientation.y,
-                            bbox.pose.orientation.z,
-                            bbox.pose.orientation.w,
-                        ),
-                        PyKDL.Vector(
-                            bbox.pose.position.x,
-                            bbox.pose.position.y,
-                            bbox.pose.position.z,
-                        )
-                    )
-                    kdlframe_clicked_point_to_box_pose = \
-                        kdlframe_clicked_frame_to_clicked_point.Inverse() \
-                        * kdlframe_clicked_frame_to_bbox_frame \
-                        * kdlframe_bbox_frame_to_bbox_pose
-                    return kdlframe_clicked_point_to_box_pose.p.Norm()
-
-                closest_index = msg_bbox_array.boxes.index(
-                    min(msg_bbox_array.boxes,
-                        key=calc_distance_to_clicked_point)
-                )
-                if calc_distance_to_clicked_point(msg_bbox_array.boxes[closest_index]) > self.param_target_search_distance:
-                    rospy.logwarn('cannot find object')
-                    return
-
-                self.target_track_id = msg_obj_array.objects[closest_index].id
-                self.point_clicked = None
-                self.time_clicked = None
+                if closest_index is not None:
+                    self.target_track_id = msg_obj_array.objects[closest_index].id
+                    self.target_pose = None
+                    self.point_clicked = None
+                    self.time_clicked = None
+                    rospy.loginfo('Find target with {}'.format(self.target_track_id))
 
         if self.target_track_id is not None:
             if self.target_track_id in [obj.id for obj in msg_obj_array.objects]:
@@ -120,6 +138,10 @@ class TrackSinglePersonNode(object):
                     self.target_track_id)
                 bbox = msg_bbox_array.boxes[target_index]
                 obj = msg_obj_array.objects[target_index]
+                self.target_point = PointStamped(
+                        header=Header(frame_id=bbox.header.frame_id),
+                        point=bbox.pose.position
+                        )
                 bbox.header.stamp = rospy.Time.now()
                 self.pub_bbox.publish(bbox)
                 self.time_target_track_lost = None
@@ -130,8 +152,20 @@ class TrackSinglePersonNode(object):
                     if rospy.Time.now() > self.time_target_track_lost + self.param_target_track_duration:
                         rospy.logerr('Lost ID: {}'.format(
                             self.target_track_id))
-                        self.target_track_id = None
-                        self.time_target_track_lost = None
+
+                        closest_index = self.get_nearest_bbox_index(
+                            self.target_point,
+                            msg_bbox_array,
+                            self.param_target_search_distance
+                            )
+
+                        if closest_index is None:
+                            self.target_track_id = None
+                            self.target_point = None
+                            self.time_target_track_lost = None
+                        else:
+                            self.target_track_id = msg_obj_array.objects[closest_index].id
+                            rospy.loginfo('Find a new target with {}'.format(self.target_track_id))
 
 
 if __name__ == '__main__':
