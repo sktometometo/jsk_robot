bc823d2d6f24002f17e4360632cdbd16f1dec5d3
Koki Shinjo <sktometometo@gmail.com>
2023-12-01 19:02:04 +0900

    [jsk_spot_startup][sample_elevator] fix app

--- jsk_spot_robot/jsk_spot_startup/apps/sample_elevator/sample_elevator.py
+++ jsk_spot_robot/jsk_spot_startup/apps/sample_elevator/sample_elevator.py
@@ -9,24 +9,28 @@ from jsk_spot_behavior_msgs.msg import NavigationGoal
 
 if __name__ == '__main__':
 
-    rospy.init_node("sample_elevator")
+    rospy.init_node("sample_navigation_with_behaviors")
     client = SpotRosClient()
 
     behavior_client = actionlib.SimpleActionClient("/spot_behavior_manager_server/execute_behaviors", NavigationAction)
     behavior_client.wait_for_server()
 
+    start_node = rospy.get_param('~start_node', 'eng2_73B2_dock')
+    target_node = rospy.get_param('~target_node', 'eng2_3FElevator')
+    dock_id = int(rospy.get_param('~dock_id', 521))
+
     client.claim()
     client.power_on()
     client.undock()
 
-    client.reset_current_node('eng2_73B2_dock')
-    client.execute_behaviors(target_node_id="eng2_3FElevator")
+    client.reset_current_node(start_node)
+    client.execute_behaviors(target_node_id=target_node)
     client.wait_execute_behaviors_result()
     result = client.get_execute_behaviors_result()
     rospy.loginfo("Result forward: {} {}".format(result.success, result.message))
 
-    client.execute_behaviors(NavigationGoal(target_node_id="eng2_73B2_dock"))
+    client.execute_behaviors(NavigationGoal(target_node_id=start_node))
     result = client.get_execute_behaviors_result()
     rospy.loginfo("Result backward: {} {}".format(result.success, result.message))
 
-    client.dock(521)
+    client.dock(dock_id)
