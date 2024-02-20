2aa529e8c19d4bdad2fe93332a5727cfb0d84240
Koki Shinjo <sktometometo@gmail.com>
2024-02-20 16:35:27 +0900

    [jsk_spot_behavior_manager][jsk_spot_beahvior_msgs] update

--- jsk_spot_robot/jsk_spot_behavior_manager/src/jsk_spot_behavior_manager/behavior_graph.py
+++ jsk_spot_robot/jsk_spot_behavior_manager/src/jsk_spot_behavior_manager/behavior_graph.py
@@ -1,99 +1,112 @@
 # -*- coding: utf-8 -*-
 
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
+
 import networkx as nx
 
+import jsk_spot_behavior_msgs.msg
 
+
+@dataclass
 class GraphEdge:
+    node_id_from: str
+    node_id_to: str
+    behavior_type: str
+    cost: int
+    properties: Dict
 
-    def __init__(self,
-                 node_id_from,
-                 node_id_to,
-                 behavior_type,
-                 cost,
-                 properties
-                 ):
-        self.node_id_from = node_id_from
-        self.node_id_to = node_id_to
-        self.behavior_type = behavior_type
-        self.cost = cost
-        self.properties = properties
+    @classmethod
+    def from_config(cls, config: Dict):
+        return cls(
+            config["from"],
+            config["to"],
+            config["behavior_type"],
+            int(config["cost"]) if "cost" in config else 1,
+            config["args"] if "args" in config else {},
+        )
+
+    @classmethod
+    def from_rosmsg(cls, msg: jsk_spot_behavior_msgs.msg.GraphEdge):
+        try:
+            args = eval(msg.args)
+        except Exception:
+            args = {}
+        return cls(
+            msg.node_id_from,
+            msg.node_id_to,
+            msg.behavior_type,
+            msg.cost,
+            args,
+        )
+
+    def to_config(self):
+        return {
+            "from": self.node_id_from,
+            "to": self.node_id_to,
+            "behavior_type": self.behavior_type,
+            "cost": self.cost,
+            "args": self.properties,
+        }
+
+    def to_rosmsg(self):
+        return jsk_spot_behavior_msgs.msg.GraphEdge(
+            node_id_from=self.node_id_from,
+            node_id_to=self.node_id_to,
+            behavior_type=self.behavior_type,
+            cost=self.cost,
+            args=str(self.properties),
+        )
 
 
+@dataclass
 class GraphNode:
-
-    def __init__(self,
-                 node_id,
-                 properties
-                 ):
-        self.node_id = node_id
-        self.properties = properties
+    node_id: str
+    properties: Dict
 
 
 class BehaviorGraph:
-
     # 現在の BehaviorGraph の仕様
     #   重み付きの有向グラフ
     #   あるノードからあるノードまでのエッジの数は 0 or 1
 
-    def __init__(self, raw_edges=[], raw_nodes={}):
-
-        self.edges = {}
-        self.nodes = {}
+    def __init__(self, raw_edges: List[Dict] = [], raw_nodes: Dict[str, Dict] = {}):
+        self.edges: Dict[Tuple[str, str], GraphEdge] = {}
+        self.nodes: Dict[str, GraphNode] = {}
         self.network = nx.DiGraph()
 
         for raw_edge in raw_edges:
-            self.add_edge(raw_edge['from'],
-                          raw_edge['to'],
-                          raw_edge['behavior_type'],
-                          int(raw_edge['cost']),
-                          raw_edge['args'])
+            self.add_edge(GraphEdge.from_config(raw_edge))
 
         for key, raw_node in raw_nodes.items():
-            self.add_node(key, raw_node)
+            self.add_node(GraphNode(key, raw_node))
 
-    def calcPath(self, node_id_from, node_id_to):
-
+    def calc_path(self, node_id_from: str, node_id_to: str):
         try:
-            node_id_list = nx.shortest_path(
-                self.network, node_id_from, node_id_to)
-        except nx.NetworkXNoPath as e:
+            node_id_list = nx.shortest_path(self.network, node_id_from, node_id_to)
+        except nx.NetworkXNoPath:
             return None
         path = []
-        for index in range(len(node_id_list)-1):
-            path.append(self.edges[node_id_list[index], node_id_list[index+1]])
+        for index in range(len(node_id_list) - 1):
+            path.append(self.edges[node_id_list[index], node_id_list[index + 1]])
         return path
 
-    def add_node(self,
-                 node_id,
-                 properties):
-        # TODO: もしすでにnodeがあれば,適切に上書きするようにする
-        # NOTICE: node と edge で整合性を保つようには実装されていない
-        self.nodes[node_id] = GraphNode(node_id, properties)
+    def add_node(self, node: GraphNode):
+        self.nodes[node.node_id] = node
 
-    def add_edge(self,
-                 node_id_from,
-                 node_id_to,
-                 behavior_type,
-                 cost,
-                 args):
-        # TODO: もしすでにedgeがあれば,適切に上書きするようにする
-        # NOTICE: node と edge で整合性を保つようには実装されていない
-        edge = GraphEdge(node_id_from,
-                         node_id_to,
-                         behavior_type,
-                         cost,
-                         args)
-        self.edges[node_id_from, node_id_to] = edge
-        self.network.add_edge(node_id_from,
-                              node_id_to,
-                              weight=cost)
+    def remove_node(self, node_id: str):
+        del self.nodes[node_id:str]
 
-    def remove_node(self,
-                    node_id):
-        del self.nodes[node_id]
+    def add_edge(self, edge: GraphEdge):
+        self.edges[edge.node_id_from, edge.node_id_to] = edge
+        self.network.add_edge(edge.node_id_from, edge.node_id_to, weight=edge.cost)
 
-    def remove_edge(self,
-                    node_id_from,
-                    node_id_to):
+    def remove_edge(self, node_id_from: str, node_id_to: str):
         del self.edges[node_id_from, node_id_to]
         self.network.remove_edge(node_id_from, node_id_to)
+
+    def get_node(self, node_id) -> GraphNode:
+        return self.nodes[node_id]
+
+    def get_edge(self, node_id_from: str, node_id_to: str) -> GraphEdge:
+        return self.edges[node_id_from, node_id_to]
--- jsk_spot_robot/jsk_spot_behavior_manager/src/jsk_spot_behavior_manager/behavior_manager_node.py
+++ jsk_spot_robot/jsk_spot_behavior_manager/src/jsk_spot_behavior_manager/behavior_manager_node.py
@@ -5,40 +5,42 @@ import sys
 import traceback
 
 import actionlib
-import rospy
-import roslaunch
 import PyKDL
-
+import roslaunch
+import rospy
+from jsk_spot_behavior_msgs.msg import (
+    NavigationAction,
+    NavigationActionFeedback,
+    NavigationFeedback,
+    NavigationResult,
+)
 from sound_play.libsoundplay import SoundClient
-from spot_ros_client.libspotros import SpotRosClient
-
-from jsk_spot_behavior_manager.behavior_graph import BehaviorGraph
-from jsk_spot_behavior_manager.base_behavior import BaseBehavior, load_behavior_class
-
 from std_msgs.msg import String
-from jsk_spot_behavior_msgs.msg import NavigationAction, NavigationFeedback, NavigationResult, NavigationActionFeedback
+
+from jsk_spot_behavior_manager.base_behavior import BaseBehavior, load_behavior_class
+from jsk_spot_behavior_manager.behavior_graph import BehaviorGraph
 from jsk_spot_behavior_msgs.srv import ResetCurrentNode, ResetCurrentNodeResponse
+from spot_ros_client.libspotros import SpotRosClient
 
 
 class BehaviorManagerNode(object):
-
     def __init__(self):
-
         # navigation dictonary
-        raw_edges = rospy.get_param('~map/edges')
-        raw_nodes = rospy.get_param('~map/nodes')
+        raw_edges = rospy.get_param("~map/edges")
+        raw_nodes = rospy.get_param("~map/nodes")
         self.graph = BehaviorGraph(raw_edges, raw_nodes)
-        self.current_node_id = rospy.get_param('~initial_node_id')
+        self.current_node_id = rospy.get_param("~initial_node_id")
         self.pre_edge = None
         self.anchor_pose = None
 
         # silent mode / dry mode
-        self.silent_mode = rospy.get_param('~silent_mode', False)
-        self.dry_mode = rospy.get_param('~dry_mode', False)
+        self.silent_mode = rospy.get_param("~silent_mode", False)
+        self.dry_mode = rospy.get_param("~dry_mode", False)
 
         # get target action name list for synchronizer
         self.list_action_name_synchronizer = rospy.get_param(
-            '~action_names_synchronizer', [])
+            "~action_names_synchronizer", []
+        )
 
         # action clients
         if self.dry_mode:
@@ -48,19 +50,20 @@ class BehaviorManagerNode(object):
             self.spot_client = SpotRosClient()
             self.sound_client = SoundClient(
                 blocking=False,
-                sound_action='/robotsound_jp',
-                sound_topic='/robotsound_jp'
+                sound_action="/robotsound_jp",
+                sound_topic="/robotsound_jp",
             )
 
         # publisher
         self.pub_current_node_id = rospy.Publisher(
-            '~current_node_id', String, queue_size=1)
+            "~current_node_id", String, queue_size=1
+        )
 
         # reset service
         self.service_reset_current_node_id = rospy.Service(
-            '~reset_current_node_id',
+            "~reset_current_node_id",
             ResetCurrentNode,
-            self.handler_reset_current_node_id
+            self.handler_reset_current_node_id,
         )
 
         #
@@ -73,35 +76,36 @@ class BehaviorManagerNode(object):
         self.list_behaviors_execution_actions = []
         for action_name in self.list_action_name_synchronizer:
             self.list_behaviors_execution_actions.append(
-                    rospy.Subscriber('{}/feedback'.format(action_name), NavigationActionFeedback, self.callback_synchronizer)
-                    )
+                rospy.Subscriber(
+                    "{}/feedback".format(action_name),
+                    NavigationActionFeedback,
+                    self.callback_synchronizer,
+                )
+            )
 
         # action server
         self.server_execute_behaviors = actionlib.SimpleActionServer(
-            '~execute_behaviors',
+            "~execute_behaviors",
             NavigationAction,
             execute_cb=self.handler_execute_behaviors,
-            auto_start=False
+            auto_start=False,
         )
         self.server_execute_behaviors.start()
 
-        rospy.loginfo('Initialized!')
+        rospy.loginfo("Initialized!")
 
     def say(self, message, blocking=True):
-
-        rospy.loginfo('Saying: {}'.format(message))
+        rospy.loginfo("Saying: {}".format(message))
         if not self.silent_mode and not self.dry_mode:
             self.sound_client.say(message, blocking=blocking)
 
     def set_anchor_pose(self):
-
-        rospy.loginfo('Set new anchor pose')
+        rospy.loginfo("Set new anchor pose")
         if not self.dry_mode:
             self.anchor_pose = self.spot_client.get_robot_pose()
 
     def go_back_to_anchor_pose(self):
-
-        rospy.loginfo('Going back to an anchor pose')
+        rospy.loginfo("Going back to an anchor pose")
         if not self.dry_mode:
             current_pose = self.spot_client.get_robot_pose()
             if current_pose is None or self.anchor_pose is None:
@@ -110,76 +114,84 @@ class BehaviorManagerNode(object):
             self.spot_client.go_pose(frame_current_to_anchor)
 
     def callback_synchronizer(self, msg):
-
-        rospy.loginfo('Current node is updated to {}'.format(msg.feedback.current_node_id))
+        rospy.loginfo(
+            "Current node is updated to {}".format(msg.feedback.current_node_id)
+        )
         self.current_node_id = msg.feedback.current_node_id
         self.pre_edge = None
 
     def run(self):
-
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
             rate.sleep()
             self.pub_current_node_id.publish(String(data=self.current_node_id))
 
     def handler_reset_current_node_id(self, req):
-
-        rospy.loginfo('current_node_id is reset to {}'.format(
-            req.current_node_id))
+        rospy.loginfo("current_node_id is reset to {}".format(req.current_node_id))
         self.current_node_id = req.current_node_id
         self.pre_edge = None
         self.set_anchor_pose()
         return ResetCurrentNodeResponse(success=True)
 
     def handler_execute_behaviors(self, goal):
-
-        rospy.loginfo('Behavior Action started. goal: {}'.format(goal))
+        rospy.loginfo("Behavior Action started. goal: {}".format(goal))
 
         current_graph = copy.deepcopy(self.graph)
         while True:
             # path calculation
-            path = current_graph.calcPath(
-                self.current_node_id, goal.target_node_id)
+            path = current_graph.calc_path(self.current_node_id, goal.target_node_id)
             if path is None:
-                rospy.logerr('No path from {} to {}'.format(
-                    self.current_node_id, goal.target_node_id))
-                self.say('パスが見つかりませんでした')
-                result = NavigationResult(
-                    success=False, message='No path found')
+                rospy.logerr(
+                    "No path from {} to {}".format(
+                        self.current_node_id, goal.target_node_id
+                    )
+                )
+                self.say("パスが見つかりませんでした")
+                result = NavigationResult(success=False, message="No path found")
                 self.server_execute_behaviors.set_aborted(result)
                 return
             else:
                 # navigation of edges in the path
-                self.say('目的地に向かいます', blocking=True)
+                self.say("目的地に向かいます", blocking=True)
                 self.go_back_to_anchor_pose()
                 success_navigation = True
                 for edge in path:
-                    rospy.loginfo('Navigating Edge {}...'.format(edge))
+                    rospy.loginfo("Navigating Edge {}...".format(edge))
                     try:
                         if self.navigate_edge(edge):
-                            rospy.loginfo('Edge {} succeeded.'.format(edge))
+                            rospy.loginfo("Edge {} succeeded.".format(edge))
                             self.current_node_id = edge.node_id_to
-                            self.server_execute_behaviors.publish_feedback(NavigationFeedback(current_node_id=self.current_node_id))
+                            self.server_execute_behaviors.publish_feedback(
+                                NavigationFeedback(current_node_id=self.current_node_id)
+                            )
                             self.pre_edge = edge
                             self.set_anchor_pose()
                         else:
-                            rospy.logwarn('Edge {} failed'.format(edge))
-                            self.say(
-                                    '移動に失敗しました。経路を探索し直します。', blocking=True)
-                            self.server_execute_behaviors.publish_feedback(NavigationFeedback(current_node_id=self.current_node_id))
+                            rospy.logwarn("Edge {} failed".format(edge))
+                            self.say("移動に失敗しました。経路を探索し直します。", blocking=True)
+                            self.server_execute_behaviors.publish_feedback(
+                                NavigationFeedback(current_node_id=self.current_node_id)
+                            )
                             current_graph.remove_edge(
-                                edge.node_id_from, edge.node_id_to)
+                                edge.node_id_from, edge.node_id_to
+                            )
                             success_navigation = False
                             break
                     except Exception as e:
                         rospy.logerr(
-                            'Got an error while navigating edge {}: {}'.format(edge, sys.exc_info()))
+                            "Got an error while navigating edge {}: {}".format(
+                                edge, sys.exc_info()
+                            )
+                        )
                         traceback.print_exc()
-                        self.say('エラーが発生しました', blocking=True)
+                        self.say("エラーが発生しました", blocking=True)
                         self.pre_edge = None
                         result = NavigationResult(
                             success=False,
-                            message='Got an error while navigating edge {}: {}'.format(edge, e))
+                            message="Got an error while navigating edge {}: {}".format(
+                                edge, e
+                            ),
+                        )
                         self.server_execute_behaviors.set_aborted(result)
                         self.anchor_pose = None
                         return
@@ -187,36 +199,35 @@ class BehaviorManagerNode(object):
                 if success_navigation:
                     break
 
-        rospy.loginfo('Goal Reached!')
-        self.say('目的地に到着しました.', blocking=True)
-        result = NavigationResult(success=True, message='Goal Reached.')
+        rospy.loginfo("Goal Reached!")
+        self.say("目的地に到着しました.", blocking=True)
+        result = NavigationResult(success=True, message="Goal Reached.")
         self.server_execute_behaviors.set_succeeded(result)
         self.set_anchor_pose()
         return
 
     def navigate_edge(self, edge):
-
         # start node id validation
         if self.current_node_id != edge.node_id_from:
             rospy.logwarn(
-                'current_node_id {} does not match node_id_from of edge ({})'.format(
-                    self.current_node_id,
-                    edge.node_id_from)
+                "current_node_id {} does not match node_id_from of edge ({})".format(
+                    self.current_node_id, edge.node_id_from
+                )
             )
             return False
 
         # load behavior class
         try:
             behavior_class = load_behavior_class(edge.behavior_type)
-            behavior = behavior_class(
-                self.spot_client,
-                self.sound_client
-            )
+            behavior = behavior_class(self.spot_client, self.sound_client)
         except Exception as e:
             rospy.logerr(
-                'Failed to load and initialize behavior class: {}'.format(sys.exc_info()))
+                "Failed to load and initialize behavior class: {}".format(
+                    sys.exc_info()
+                )
+            )
             traceback.print_exc()
-            self.say('行動クラスを読み込めませんでした', blocking=True)
+            self.say("行動クラスを読み込めませんでした", blocking=True)
             self.pre_edge = None
             return False
 
@@ -225,16 +236,14 @@ class BehaviorManagerNode(object):
 
         # Exception from behavior will be caught in handler
         rospy.loginfo("Running run_initial...")
-        success_initial = behavior.run_initial(
-            node_from, node_to, edge, self.pre_edge)
+        success_initial = behavior.run_initial(node_from, node_to, edge, self.pre_edge)
         if success_initial is False:
             rospy.loginfo("run_initial failed. exitting..")
             behavior.run_final(node_from, node_to, edge, self.pre_edge)
             return False
         else:
             rospy.loginfo("running run_main...")
-            success_main = behavior.run_main(
-                node_from, node_to, edge, self.pre_edge)
+            success_main = behavior.run_main(node_from, node_to, edge, self.pre_edge)
             rospy.loginfo("running run_final")
             behavior.run_final(node_from, node_to, edge, self.pre_edge)
             return success_main
