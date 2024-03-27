40ccad63bfea72b30f4cc204c7cc14bfb1948db2
Koki Shinjo <sktometometo@gmail.com>
2024-03-27 19:02:18 +0900

    updat

--- jsk_spot_robot/jsk_spot_startup/node_scripts/static_tf_republisher.py
+++ jsk_spot_robot/jsk_spot_startup/node_scripts/static_tf_republisher.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-import sys
 import argparse
+import sys
 
-import rospy
 import rosbag
+import rospy
 import tf2_ros
-
 from geometry_msgs.msg import TransformStamped
 from tf2_msgs.msg import TFMessage
 
 
 def main():
 
-    rospy.init_node('static_tf_republisher')
+    rospy.init_node("static_tf_republisher")
 
-    topicname = '/tf_static'
+    topicname = "/tf_static"
 
     myargv = rospy.myargv(argv=sys.argv)
     if len(myargv) > 1:
@@ -27,16 +26,17 @@ def main():
 
     list_messages = []
     transforms = []
-    with rosbag.Bag(bagfilename, 'r') as inputbag:
-        for topic, msg, t in inputbag.read_messages('/tf_static'):
+    with rosbag.Bag(bagfilename, "r") as inputbag:
+        for topic, msg, t in inputbag.read_messages("/tf_static"):
             transforms.extend(msg.transforms)
 
     rospy.loginfo(
-        'republish /tf_static with {} TransformStamped messages'.format(len(transforms)))
+        "republish /tf_static with {} TransformStamped messages".format(len(transforms))
+    )
 
     broadcaster.sendTransform(transforms)
     rospy.spin()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
