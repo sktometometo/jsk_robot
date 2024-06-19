dd8a40df7f5f9412ce6f6f4cb56bb80bb02cc97b
Your Name <you@example.com>
2024-06-19 14:24:06 +0900

    updat

--- jsk_spot_robot/jsk_spot_startup/node_scripts/faults_speaker.py
+++ jsk_spot_robot/jsk_spot_startup/node_scripts/faults_speaker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
+from queue import Queue
+import threading
 from sound_play.libsoundplay import SoundClient
 from spot_msgs.msg import SystemFaultState
 
@@ -17,15 +19,26 @@ class FaultsSpeaker:
                 SystemFaultState,
                 self._cb
                 )
+        self.queue = []
+        self.lock = threading.Lock()
+        threading.Thread(target=self._speaking_thread).start()
+
+    def _speaking_thread(self):
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            rate.sleep()
+            with self.lock:
+                if len(self.queue) > 0:
+                    message = self.queue.pop(0)
+                    self.client.say(message, blocking=False)
 
     def _cb(self, msg):
         for fault in msg.faults:
             if fault.severity >= self._severity_level:
-                self.client.say(
-                        "I have got an error {}.".format(fault.name) \
-                                + "{}".format(fault.error_message),
-                        blocking=True
-                        )
+                message = "System error {}.".format(fault.name) + "{}".format(fault.error_message)
+                with self.lock:
+                    if message not in self.queue:
+                        self.queue.append(message)
 
 
 if __name__ == '__main__':
